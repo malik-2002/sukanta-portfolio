04c1f894350b9d3060ed117b12345f4ced5b6220
Sukanta Malik <sukantamalik2002@gmail.com>
2023-10-12 21:31:21 +0530

    Changes Of all section

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ var btn01=document.getElementById("btn01");
 var btn02=document.getElementById("btn02");
 btn01.addEventListener("click",()=>{
     btn02.classList.toggle('hidden');
+});
+
+var hire=document.getElementById('hire');
+hire.addEventListener("click",()=>{
+    alert("Email: sukantamalik2002@gmail.com")
 })
