c85ca97521e8afe96200c525de9407d1892af346
Sukanta Malik <sukantamalik2002@gmail.com>
2023-10-13 16:46:55 +0530

    Add toast file

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 var btn01=document.getElementById("btn01");
 var btn02=document.getElementById("btn02");
+var hide_toast = document.getElementById("hide_toast");
+var hide = document.getElementById("hide");
+var add_text = document.getElementById("add_text");
+var needUpdate = true
+hide_toast.addEventListener("click", () => {
+    hide.classList.add('hidden');
+})
 btn01.addEventListener("click",()=>{
     btn02.classList.toggle('hidden');
 });
-
 var hire=document.getElementById('hire');
 hire.addEventListener("click",()=>{
-    alert("Email: sukantamalik2002@gmail.com")
+    hide.classList.remove('hidden');
+    if(needUpdate){
+        const v = document.createTextNode("Email:sukantamalik2002@gmail.com");
+        add_text.appendChild(v);
+        needUpdate=false;
+    }
+    
 })
 
 var typed = new Typed('#element', {
